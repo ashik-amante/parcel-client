db971974b52c6cb7663f76c68001b9b8c1d16c82
Abdullah Al Ashik <abdullahashik17@gmail.com>
2025-10-02 11:35:48 +0600

    remove banner carousel legend

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -11,15 +11,15 @@ const Banner = () => {
              <Carousel autoPlay={true} infiniteLoop={true} showThumbs={false}>
                 <div>
                     <img src={bn1} />
-                    <p className="legend">Legend 1</p>
+                   
                 </div>
                 <div>
                     <img src={bn2}/>
-                    <p className="legend">Legend 2</p>
+                    
                 </div>
                 <div>
                     <img src={bn3} />
-                    <p className="legend">Legend 3</p>
+                    
                 </div>
             </Carousel>
         </div>
